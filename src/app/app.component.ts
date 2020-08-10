251dfaef966b753b2633be2f6d33cbc63ebfabef
wilsonmariano <mgw009@gmail.com>
2020-08-10 16:11:54 -0300

    Agrego navbar. Agrego carousel. Agrego section de info. Agrego cards con opciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faEdit, faEraser } from '@fortawesome/free-solid-svg-icons';
+import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'diaDelNinoSindicato';
+export class AppComponent implements OnInit {
+  public faEdit =  faEdit;
+  public faAddressCard = faAddressCard;
+  public faEraser = faEraser;
+
+  ngOnInit() {
+
+  }
 }
